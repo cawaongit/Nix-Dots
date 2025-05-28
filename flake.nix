a30fb8cf2b1cb6d242fe843dda4ecc93e21e52be
cawaongit <sasha.martinhernandez@gmail.com>
2025-05-28 14:55:26 +0200

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, nixpkgs-unstable, ... } @ inputs:
+  {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
---
+++ hosts/laptop/configuration.nix
@@ -0,0 +1,225 @@
+
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, nixpkgs, nixpkgs-unstable, lib, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+      };
+    };
+  };
+
+  networking = {
+    hostName = "nixos";
+    networkmanager = {
+      enable = true;
+    };
+  };
+
+  # Set your time zone.
+  time = {
+    timeZone = "Europe/Brussels";
+  };
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "fr_BE.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services = {
+    getty = {
+      autologinUser = "sasha";
+    };
+    blueman = {
+      enable = true;
+    };
+    udisks2 = {
+      enable = true;
+    };
+    pipewire = {
+      enable = true;
+      alsa = {
+          enable = true;
+	  support32Bit = true;
+	};
+	pulse = {
+          enable = true;
+	};
+    };
+    xserver = {
+      xkb = {
+        layout = "be";
+	variant = "nodeadkeys";
+      };
+      videoDrivers = ["hardware.nvidia.open"];
+    };
+  };
+
+  # Configure console keymap
+  console = {
+    keyMap = "be-latin1";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users = {
+    users = {
+      sasha = {
+        isNormalUser = true;
+	description = "Sasha";
+	extraGroups = [ "networkmanager" "wheel" ];
+	packages = with pkgs; [];
+      };
+    };
+    groups = {
+      libvirtd = {
+        members = ["sasha"];
+      };
+    };
+  };
+
+  # Allow unfree packages
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment = {
+    systemPackages = with pkgs; [
+      neovim
+      kitty
+      firefox-devedition-bin
+      rofi-wayland
+      discord
+      hyprlock
+      waybar
+      libreoffice-qt6-fresh
+      unzip
+      spotify
+      libgcc
+      mangohud
+      neofetch
+      luarocks
+      gcc
+      teams-for-linux
+      superfile
+      lazygit
+      fd
+      python3
+      hyprpaper
+      pavucontrol
+      catppuccin-cursors.mochaPeach
+      curl
+      hyprcursor
+      texliveBasic
+      playerctl
+      ciscoPacketTracer8
+    ];
+  };
+
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+
+  programs = {
+    hyprland = {
+      enable = true;
+    };
+    steam = {
+      enable = true;
+      gamescopeSession = {
+          enable = true;
+      };
+    };
+    git = {
+      enable = true;
+    };
+    gamemode = {
+      enable = true;
+    };
+    virt-manager = {
+      enable = true;
+    };
+  };
+
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.fantasque-sans-mono
+      nerd-fonts.jetbrains-mono
+      font-awesome
+    ];
+  };
+
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+    };
+  };
+
+  virtualisation = {
+    libvirtd.enable = true;
+    spiceUSBRedirection.enable = true;
+  };
+
+  security = {
+    rtkit = {
+      enable = true;
+    };
+  };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # Did you read the comment?
+  system = {
+    stateVersion = "25.05";
+  }; #Did you read the comment?
+}
